04df84b1ca72d4b8defc032d12ebd296a1457896
Bruno Moreira <bruno@terraduo.com>
2023-12-28 13:37:17 +0000

    Adds public Sentry DSN.

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -5,7 +5,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-    dsn: process.env["NEXT_PUBLIC_SENTRY_DSN"],
+    dsn: process.env["NEXT_PUBLIC_SENTRY_DSN"] as string,
     enabled: process.env["NODE_ENV"] === "production",
     tracesSampleRate: 0.1,
     debug: false,
