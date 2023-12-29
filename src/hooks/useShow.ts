a874778344177f818acb9f3e2cb8d517c53f59e7
Bruno Moreira <bruno@terraduo.com>
2023-12-29 12:32:10 +0000

    Adds error handling for when fetching show details.

--- src/hooks/useShow.ts
+++ src/hooks/useShow.ts
@@ -2,6 +2,7 @@ import { getShowState } from "@/server/getShowState";
 import { useCallback, useEffect, useState } from "react";
 import type { ShowSummary } from "@/utils/types";
 import type { GetShowDetailsResponse } from "@/server/getShowDetails";
+import * as Sentry from "@sentry/nextjs";
 
 export default function useShow(matrixId: string, showId: string) {
     const [show, setShow] = useState<ShowSummary>();
@@ -11,28 +12,34 @@ export default function useShow(matrixId: string, showId: string) {
     const [details, setDetails] = useState<GetShowDetailsResponse>();
 
     const refreshShowState = useCallback(async () => {
-        const { show, episodesSynced, episodeCount, synced, details } = await getShowState(matrixId, showId);
-        setShow(show);
-        setEpisodesSynced(episodesSynced);
-        setSynced(synced);
+        try {
+            const { show, episodesSynced, episodeCount, synced, details } = await getShowState(matrixId, showId);
+            setShow(show);
+            setEpisodesSynced(episodesSynced);
+            setSynced(synced);
 
-        if (episodeCount) {
-            setEpisodeCount(episodeCount);
-        }
-
-        if (details) {
-            setDetails(details);
-        }
-
-        if (!synced) {
-            setTimeout(refreshShowState, 250);
-        } else if (details) {
-            // Check if any episodes are still missing plot points:
-            const missingPlotPoints = details.show.seasons.flatMap((season) => season.episodes.filter((episode) => episode.plot_points.length === 0));
-
-            if (missingPlotPoints.length > 0) {
-                setTimeout(refreshShowState, 2000);
+            if (episodeCount) {
+                setEpisodeCount(episodeCount);
             }
+
+            if (details) {
+                setDetails(details);
+            }
+
+            if (!synced) {
+                setTimeout(refreshShowState, 250);
+            } else if (details) {
+                // Check if any episodes are still missing plot points:
+                const missingPlotPoints = details.show.seasons.flatMap((season) => season.episodes.filter((episode) => episode.plot_points.length === 0));
+
+                if (missingPlotPoints.length > 0) {
+                    setTimeout(refreshShowState, 2000);
+                }
+            }
+        } catch (error) {
+            // We don't want to crash the app if we can't get the show state. Just try again in a bit.
+            Sentry.captureException(error);
+            setTimeout(refreshShowState, 2000);
         }
     }, [matrixId, showId]);
 
