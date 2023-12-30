0955df70ab7643583e2de57d532f19653270b9d8
Bruno Moreira <bruno@terraduo.com>
2023-12-30 20:59:33 +0000

    Improves the appearance of the footer on mobile.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { intervalToDuration } from "date-fns";
+import clsx from "clsx";
 
 type Props = {
     comparisonsLeft?: number;
@@ -22,14 +23,16 @@ export default function Footer({ comparisonsLeft }: Props) {
         <div className="w-full mx-auto bg-persian-900 p-4">
             <div className="text-white text-center text-xs sm:text-sm">
                 {comparisonsLeft !== undefined && <>~{formattedDuration} left &middot; </>}
-                Built with 🩵 by{" "}
+                Built <span className={clsx(comparisonsLeft ? "hidden sm:inline-block" : "")}>with 🩵</span> by{" "}
                 <Link href="https://pocketarc.com" className="underline underline-offset-4">
                     PocketArC
                 </Link>{" "}
-                with data from{" "}
-                <Link href="https://www.themoviedb.org" className="underline underline-offset-4">
-                    TMDB
-                </Link>{" "}
+                <span className={clsx(comparisonsLeft ? "hidden sm:inline-block" : "")}>
+                    with data from{" "}
+                    <Link href="https://www.themoviedb.org" className="underline underline-offset-4">
+                        TMDB
+                    </Link>{" "}
+                </span>
                 &middot;{" "}
                 <Link href="/about" className="underline underline-offset-4">
                     About
