f7adf9d0c3ef75a3a9a64369686bad9735e5b7a2
Bruno Moreira <git@pocketarc.com>
2025-12-28 16:00:23 -0700

    feat: Add security headers and Content Security Policy to next.config.ts.

--- next.config.ts
+++ next.config.ts
@@ -35,6 +35,39 @@ const nextConfig: NextConfig = {
             destination: "https://plausible.io/api/event",
         },
     ],
+    headers: async () => [
+        {
+            source: "/:path*",
+            headers: [
+                { key: "X-Frame-Options", value: "SAMEORIGIN" },
+                { key: "Strict-Transport-Security", value: "max-age=31536000; includeSubDomains" },
+                { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
+                { key: "Cross-Origin-Opener-Policy", value: "same-origin" },
+                { key: "Cross-Origin-Embedder-Policy", value: "credentialless" },
+                { key: "Cross-Origin-Resource-Policy", value: "same-origin" },
+                { key: "Permissions-Policy", value: "autoplay=(self), cross-origin-isolated=(self)" },
+                { key: "X-Content-Type-Options", value: "nosniff" },
+                {
+                    key: "Content-Security-Policy",
+                    value: [
+                        "default-src 'self'",
+                        "script-src 'self' 'unsafe-inline' *.ingest.sentry.io static.cloudflareinsights.com",
+                        "style-src 'self' 'unsafe-inline'",
+                        "connect-src 'self' *.ingest.sentry.io cloudflareinsights.com",
+                        "img-src 'self'",
+                        "font-src 'self'",
+                        "frame-src 'self'",
+                        "object-src 'none'",
+                        "base-uri 'self'",
+                        "manifest-src 'self'",
+                        "media-src 'self'",
+                        "worker-src 'none'",
+                        "report-uri https://o39003.ingest.sentry.io/api/4506473537142784/security/?sentry_key=edf14ed7586516238165feee59340cea",
+                    ].join("; "),
+                },
+            ],
+        },
+    ],
     experimental: {
         serverActions: {
             // Disable server actions; they are a security risk that isn't worth it.
